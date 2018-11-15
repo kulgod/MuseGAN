ca028240dee387ecd0bc0dfae01efc428c4f14c7
neelyerneni <nesh4646@gmail.com>
2018-11-14 18:09:28 -0800

    model fwk

---
+++ model.py
@@ -0,0 +1,27 @@
+import json
+import numpy as np
+from torch import autograd, nn, optim
+from torch.nn import functional as F
+import torch
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+class Classifier(nn.Module):
+    def __init__(self):
+        pass
+
+    def classify(self):
+        pass
